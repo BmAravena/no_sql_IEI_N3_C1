053c2f861e4a01df1f24ed9e71beac0be45bfb7d
baileytorch <baileytorch@hotmail.com>
2026-05-25 10:52:40 -0400

    Actualizando JS

--- js/form.js
+++ js/form.js
@@ -4,5 +4,13 @@ window.onload = function(){
 
 function validarFormulario(){
     let inputNombre = document.getElementById('inputNombre').value;
-    alert(inputNombre + '');
+    let inputRut = document.getElementById('inputRut').value;
+    let inputEmail = document.getElementById('inputEmail').value;
+    let inputContrasena = document.getElementById('password').value;
+    
+    if(inputNombre == '' || inputRut == '' || inputEmail == '' || inputContrasena == ''){
+        alert('Por favor ingrese TODOS los datos requeridos.')
+    }else{
+        alert('datos correctos, procesando formulario...')
+    }
 };

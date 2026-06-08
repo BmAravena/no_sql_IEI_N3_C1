f389f84b52130c5181730d184b6cf743cad33655
baileytorch <baileytorch@hotmail.com>
2026-06-08 09:24:16 -0400

    INSERCIÓN DATOS

--- app/js/form.js
+++ app/js/form.js
@@ -10,15 +10,15 @@ function validarFormulario() {
         formularioValido = false;
     }
 
-    if (!validarInput(inputRut)) {
+    if (!validarInput(inputRut) || !validarRut(inputRut)) {
         formularioValido = false;
     }
 
-    if (!validarInput(inputEmail) || !validarEmail(inputEmail.value)) {
+    if (!validarInput(inputEmail) || !validarEmail(inputEmail)) {
         formularioValido = false;
     }
 
-    if (!validarInput(inputContrasena) || !validarContrasena(inputContrasena.value)) {
+    if (!validarInput(inputContrasena) || !validarContrasena(inputContrasena)) {
         formularioValido = false;
     }
 
@@ -27,30 +27,68 @@ function validarFormulario() {
     }
 
     if (formularioValido) {
-        alert('Datos ingresados correctamente, enviando formulario...');
+        const formulario = document.getElementById('formularioRegistro');
+        const dataForm = new FormData(formulario);
+        const datos = Object.fromEntries(dataForm.entries());
+
+        const response = async () => {
+            await fetch('http://localhost:5000/guardarUsuario', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(datos)
+            }).then(response => response.json())
+                .then(data => console.log("Datos recividos:", data))
+                .catch(error => console.error("Error:", error))
+        }
+        console.log(response.json())
     } else {
         alert('Debe completar los campos resaltados para enviar el formulario');
     }
 };
 
 function validarInput(input) {
-    if (input.value == '') {
-        input.classList.add('alerta');
-        input.classList.add('is-invalid');
-    }
-    else{
-        input.classList.remove('alerta');
-        input.classList.remove('is-invalid');
-        input.classList.add('is-valid');
-    }
+    input.value == '' ? inputInvalido(input) : inputValido(input);
 }
 
-function validarEmail(email) {
+function validarEmail(inputEmail) {
     const expresionEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    return expresionEmail.test(email);
+    expresionEmail.test(inputEmail.value) ? inputValido(inputEmail) : inputInvalido(inputEmail);
 }
 
-function validarContrasena(contrasena) {
+function validarContrasena(inputContrasena) {
     const expresionContrasena = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
-    return expresionContrasena.test(contrasena);
+    expresionContrasena.test(inputContrasena.value) ? inputValido(inputContrasena) : inputInvalido(inputContrasena);
 }
+
+function validarRut(inputRut) {
+    rutCompleto = inputRut.value
+    if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test(rutCompleto))
+        return false;
+    var tmp = rutCompleto.split('-');
+    var digv = tmp[1];
+    var rut = tmp[0];
+    if (digv == 'K') digv = 'k';
+    digitoVerificador(rut) != digv ? inputInvalido(inputRut) : inputValido(inputRut);
+}
+
+function digitoVerificador(T) {
+    var M = 0, S = 1;
+    for (; T; T = Math.floor(T / 10))
+        S = (S + T % 10 * (9 - M++ % 6)) % 11;
+    return S ? S - 1 : 'k';
+}
+
+function inputInvalido(input) {
+    input.classList.add('alerta');
+    input.classList.add('is-invalid');
+    return false
+};
+
+function inputValido(input) {
+    input.classList.remove('alerta');
+    input.classList.remove('is-invalid');
+    input.classList.add('is-valid');
+    return true
+};
--- servidor/server.js
+++ servidor/server.js
@@ -4,19 +4,25 @@ const bodyParser = require('body-parser'); // Toma la información de un FORM y 
 const mongoose = require('mongoose'); // ORM para trabajar con express (Object Relatonal Mapping)
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 5000;;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extend: true }));
+// app.use(bodyParser.json());
+
+// Chequeamos el puerto en el que efectivamente está corriendo la app
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
 
 // Crear la conexion con MongoDB
 mongoose.connect('mongodb://localhost:27017/test')
-    .then(()=>console.log('Conexión Exitosa!'))
-    .catch((err)=>console.error('Error al conectar a la DB: ',err));
+    .then(() => console.log('Conexión Exitosa!'))
+    .catch((err) => console.error('Error al conectar a la DB: ', err));
 
-// Creamos la ENTIDAD en mongoose (ORM)
+// Creamos la ENTIDAD/MODELO en mongoose (ORM)
 const usuario = new mongoose.Schema({
     nombre: String,
     rut: String,
@@ -25,18 +31,21 @@ const usuario = new mongoose.Schema({
     contrasena: String
 });
 
-// Iniciamos el objeto en mongoose
-const Usuario = mongoose.model('Usuario',usuario,'usuarios');
+// Creamos el OBJETO en mongoose, usando el MODELO como patrón/base
+const Usuario = mongoose.model('Usuario', usuario, 'usuarios');
 
 // Crear el ENDPOINT para recibir los datos de usuario
-app.post('guardarUsuario', async(req,res)=>{
-    try{        
-        const{nombre, rut, email, fechaNacimiento, contrasena} = req.body;
-        const nuevoUsuario = new Usuario({nombre, rut, email, fechaNacimiento, contrasena});
+app.post('/guardarUsuario', async (req, res) => {
+    try {
+        // Leemos la data desde el BODY (cuerpo) de la REQUEST (solicitud)
+        const { nombre, rut, email, fechaNacimiento, contrasena } = req.body;
+        // Instanciamos el OBJETO Usuario con los valores obtenidos desde la REQUEST
+        const nuevoUsuario = new Usuario({ nombre, rut, email, fechaNacimiento, contrasena });
+
+        // Le indicamos al ORM que debe PERSISTIR ese OBJETO
         await nuevoUsuario.save();
-        res.status(200).json({message:'Datos Guardados correctamente.'});
-    }catch (err){
-        console.log('Error al guardar los datos: ',err);
-        res.status(500).json({message: 'Error al guardar los datos'});
+        res.status(200).json({ message: 'Datos Guardados correctamente.' });
+    } catch (err) {
+        res.status(500).json({ message: 'Error al guardar los datos' });
     }
 });

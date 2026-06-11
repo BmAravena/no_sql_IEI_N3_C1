3ec1f5402bd1809ac7d73befd4b27459c0cd9270
baileytorch <baileytorch@hotmail.com>
2026-06-11 17:42:58 -0400

    Obteniendo data desde DB

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,26 @@
-window.onload = function(){
-    new DataTable('#example');
+window.onload = function () {
+    obtenerUsuarios();
 }
+
+function obtenerUsuarios() {
+    const enviarFormulario = async () => {
+        try {
+            // LLamamos al ENDPOINT para obtener datos de usuarios
+            const response = await fetch('http://localhost:3000/obtenerUsuarios');
+            const usuarios = await response.json();
+
+            new DataTable('#usuarios', {
+                data: usuarios,
+                columns: [
+                    { data: 'nombre' },
+                    { data: 'rut' },
+                    { data: 'email' },
+                    { data: 'fechaNacimiento' }
+                ]
+            });
+        } catch (err) {
+            console.log('Error al obtener los datos: ', err)
+        }
+    }
+    enviarFormulario();
+};
